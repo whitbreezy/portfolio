43c66c0dffb603d044b8fdb5d97a093af298c1fc
whitbreezy <whitneybrady20@gmail.com>
2024-01-22 13:50:20 -0700

    added logo, need to test when i resolve server issues

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -4,7 +4,15 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function customNav({ links }) {
     return (
         <Navbar expand="lg" className="bg-body-tertiary p-3 mb-5">
-            <Navbar.Brand>Whitney Brady</Navbar.Brand>
+            <Navbar.Brand>
+                <img
+                    src="../assets/wblogo.png"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="custom logo"
+                />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
