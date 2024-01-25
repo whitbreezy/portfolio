f0fc7c55bb188dc12d10ba5865590811d909598b
whitbreezy <whitneybrady20@gmail.com>
2024-01-25 10:36:54 -0700

    project imgs

--- src/projects.js
+++ src/projects.js
@@ -1,42 +1,42 @@
 const projects = [
     {
         id: '1',
-        img: 'src/assets/wblogo.png',
+        img: 'src/assets/foodfeed.png',
         title: 'Foodfeed',
         url: 'https://enigmatic-hollows-51581-85d21f4c37a7.herokuapp.com/login',
         github: 'https://github.com/Phansen47/FoodFeed'
     },
     {
         id: '2',
-        img: 'src/assets/wblogo.png',
+        img: 'src/assets/getrhythm.png',
         title: 'Get Rhythm',
         url: 'https://whitbreezy.github.io/get-rhythm/',
         github: 'https://github.com/whitbreezy/get-rhythm'
     },
     {
         id: '3',
-        img: 'src/assets/wblogo.png',
+        img: 'src/assets/weather-dashboard.png',
         title: 'Weather Dashboard',
         url: 'https://whitbreezy.github.io/weather-dashboard/',
         github: 'https://github.com/whitbreezy/weather-dashboard'
     },
     {
         id: '4',
-        img: 'src/assets/wblogo.png',
-        title: 'E-Commerce Backend',
-        url: 'https://drive.google.com/file/d/10CXetC_QeFhRy3n2enK9D1E2V0ngrLaf/view',
-        github: 'https://github.com/whitbreezy/ecommerce-backend'
+        img: 'src/assets/note-taker.png',
+        title: 'Note Taker',
+        url: 'https://peaceful-badlands-58405-a2b39c56ddb2.herokuapp.com/',
+        github: 'https://github.com/whitbreezy/note-taker'
     },
     {
         id: '5',
-        img: 'src/assets/wblogo.png',
+        img: 'src/assets/JATE.png',
         title: 'Text Editor PWA',
         url: 'https://text-editor-pwa-x94e.onrender.com/',
         github: 'https://github.com/whitbreezy/text-editor-pwa'
     },
     {
         id: '6',
-        img: 'src/assets/wblogo.png',
+        img: 'src/assets/js-quiz.png',
         title: 'JavaScript Quiz Game',
         url: 'https://whitbreezy.github.io/js-quiz/',
         github: 'https://github.com/whitbreezy/js-quiz'
