adb82aeba5ae264a0f26709e34b8e00fc412d563
whitbreezy <whitneybrady20@gmail.com>
2024-02-14 16:31:12 -0700

    added latest project

--- src/projects.js
+++ src/projects.js
@@ -4,46 +4,54 @@ import weather from './assets/weather-dashboard.png'
 import noteTaker from './assets/note-taker.png'
 import JATE from './assets/JATE.png'
 import jsQuizImg from './assets/js-quiz.png'
+import tahanan from './assets/project-screenshot-tahanan'
 
 //create array of objects containing details for each project and their images
 const projects = [
     {
         id: '1',
+        img: tahanan,
+        title: 'JavaScript Quiz Game',
+        url: 'https://tahanancandleco.onrender.com/',
+        github: 'https://github.com/mjthurber/Tahanan_Candle'
+    },
+    {
+        id: '2',
         img: foodfeedimg,
         title: 'Foodfeed',
         url: 'https://enigmatic-hollows-51581-85d21f4c37a7.herokuapp.com/login',
         github: 'https://github.com/Phansen47/FoodFeed'
     },
     {
-        id: '2',
+        id: '3',
         img: getrhythmimg,
         title: 'Get Rhythm',
         url: 'https://whitbreezy.github.io/get-rhythm/',
         github: 'https://github.com/whitbreezy/get-rhythm'
     },
     {
-        id: '3',
+        id: '4',
         img: weather,
         title: 'Weather Dashboard',
         url: 'https://whitbreezy.github.io/weather-dashboard/',
         github: 'https://github.com/whitbreezy/weather-dashboard'
     },
     {
-        id: '4',
+        id: '5',
         img: noteTaker,
         title: 'Note Taker',
         url: 'https://peaceful-badlands-58405-a2b39c56ddb2.herokuapp.com/',
         github: 'https://github.com/whitbreezy/note-taker'
     },
     {
-        id: '5',
+        id: '6',
         img: JATE,
         title: 'Text Editor PWA',
         url: 'https://text-editor-pwa-x94e.onrender.com/',
         github: 'https://github.com/whitbreezy/text-editor-pwa'
     },
     {
-        id: '6',
+        id: '7',
         img: jsQuizImg,
         title: 'JavaScript Quiz Game',
         url: 'https://whitbreezy.github.io/js-quiz/',
